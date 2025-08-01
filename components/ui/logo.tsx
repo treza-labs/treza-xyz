8275efa29536c179e2107ba7c6c8d536482b0a62
treza-labs <trezalabs@gmail.com>
2025-08-01 11:00:48 -0700

    Reduce logo size in header and footer - Decreased logo image from 32x32 to 24x24 pixels - Changed text size from text-xl to text-lg - Affects both header and footer components

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -5,8 +5,8 @@ import logo from "@/public/images/logo.svg";
 export default function Logo() {
   return (
     <Link href="/" className="inline-flex shrink-0 items-center gap-3" aria-label="Treza">
-      <Image src={logo} alt="Treza Labs" width={32} height={32} />
-      <span className="text-xl font-semibold text-white tracking-widest">
+      <Image src={logo} alt="Treza Labs" width={24} height={24} />
+      <span className="text-lg font-semibold text-white tracking-widest">
         T R E Z A
       </span>
     </Link>

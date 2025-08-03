63a7747f36689aa811b95ccad6af25511089c81b
treza-labs <trezalabs@gmail.com>
2025-08-02 21:14:26 -0700

    feat: increase GitHub icon size to w-6 h-6
    - Increase GitHub SVG icon from w-5 h-5 to w-6 h-6 (24px)
    - Keep container size consistent at w-8 h-8 for all social icons
    - GitHub icon now more prominent within same circle size
    - Brandon Torres GitHub link has better visual presence

--- components/team.tsx
+++ components/team.tsx
@@ -173,7 +173,7 @@ export default function Team() {
                         >
                           {socialIcon === 'github' ? (
                             <svg
-                              className="w-5 h-5 fill-current opacity-75 hover:opacity-100 transition-opacity duration-200"
+                              className="w-6 h-6 fill-current opacity-75 hover:opacity-100 transition-opacity duration-200"
                               viewBox="0 0 32 32"
                               xmlns="http://www.w3.org/2000/svg"
                             >

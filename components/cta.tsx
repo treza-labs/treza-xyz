a33c61ccc5d925fd5c5491f9d9975a43a6bdb1c0
treza-labs <trezalabs@gmail.com>
2025-09-19 12:27:13 -0700

    Update CTA component

--- components/cta.tsx
+++ components/cta.tsx
@@ -23,9 +23,9 @@ export default function Cta() {
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-8 font-nacelle text-3xl font-semibold text-transparent md:text-4xl"
               data-aos="fade-up"
             >
-              Try Treza for free
+              Try Treza Enclaves for free
             </h2>
-            <p className="text-lg text-indigo-200/65">
+            <p className="text-lg text-indigo-200/65 pb-8">
               Get started with Treza for free. No credit card required.
             </p>
             <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
